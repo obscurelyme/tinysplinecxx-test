3dd14f2d3d2e1ec3a0b16af88f0236f6edd962e8
stdout-reach <nico.greco@outreach.io>
2021-11-21 11:10:48 -0800

    try local vcpkg

--- .github/scripts/linux/configure.sh
+++ .github/scripts/linux/configure.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cmake --no-warn-unused-cli \
-  -DCMAKE_TOOLCHAIN_FILE="~/vcpkg/scripts/buildsystems/vcpkg.cmake" \
+  -DCMAKE_TOOLCHAIN_FILE="./vcpkg/scripts/buildsystems/vcpkg.cmake" \
   -B ./build
